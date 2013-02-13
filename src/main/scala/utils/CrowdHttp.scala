095ea0b7493bb6725d74e030f0cfaf5e0cd4176a
Hirokazu NISHIOKA <hiro@nisshiee.org>
2013-02-13 22:43:58 +0900

    [ #13 ] make CrowdHttp as trait

--- src/main/scala/utils/CrowdHttp.scala
+++ src/main/scala/utils/CrowdHttp.scala
@@ -7,27 +7,46 @@ import scala.util.control.Exception.allCatch
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-object CrowdHttp {
+trait CrowdHttp {
 
-  def get(path: String, params: Map[String, String])(implicit conn: CrowdConnection) = {
-    val req = url(conn.urlPrefix + path) |>
-      (_ as (conn.appname, conn.password)) |>
-      (_ <<? params)
+  def get
+    (path: String, params: Map[String, String])
+    (implicit conn: CrowdConnection)
+    : Validation[ConnectionError, (Int, String)]
 
-    allCatch opt { Http(req > { res: Response =>
-      (res.getStatusCode, res.getResponseBody)
-    })() } toSuccess ConnectionError
-  }
+  def postJson
+    (path: String, params: Map[String, String], body: JValue)
+    (implicit conn: CrowdConnection)
+    : Validation[ConnectionError, (Int, String)]
+}
 
-  def postJson(path: String, params: Map[String, String], body: JValue)(implicit conn: CrowdConnection) = {
-    val req = url(conn.urlPrefix + path) |>
-      (_ as (conn.appname, conn.password)) |>
-      (_ <:< List("Content-Type" -> "application/json")) |>
-      (_ <<? params) |>
-      (_ << compact(body))
+object CrowdHttp extends CrowdHttp {
 
-    allCatch opt { Http(req > { res: Response =>
-      (res.getStatusCode, res.getResponseBody)
-    })() } toSuccess ConnectionError
-  }
+  override def get
+    (path: String, params: Map[String, String])
+    (implicit conn: CrowdConnection)
+    : Validation[ConnectionError, (Int, String)] = {
+      val req = url(conn.urlPrefix + path) |>
+        (_ as (conn.appname, conn.password)) |>
+        (_ <<? params)
+
+      allCatch opt { Http(req > { res: Response =>
+        (res.getStatusCode, res.getResponseBody)
+      })() } toSuccess ConnectionError
+    }
+
+  override def postJson
+    (path: String, params: Map[String, String], body: JValue)
+    (implicit conn: CrowdConnection)
+    : Validation[ConnectionError, (Int, String)] = {
+      val req = url(conn.urlPrefix + path) |>
+        (_ as (conn.appname, conn.password)) |>
+        (_ <:< List("Content-Type" -> "application/json")) |>
+        (_ <<? params) |>
+        (_ << compact(body))
+
+      allCatch opt { Http(req > { res: Response =>
+        (res.getStatusCode, res.getResponseBody)
+      })() } toSuccess ConnectionError
+    }
 }
