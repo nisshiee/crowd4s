a11da3f8a691a83d14b27a7be8621336be17452b
Hirokazu NISHIOKA <hiro@nisshiee.org>
2013-02-21 22:46:06 +0900

    [ #12 ] make AuthorizedGroup type

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -3,14 +3,15 @@ package org.nisshiee.crowd4s
 import com.typesafe.config.{ Config => TConfig, ConfigFactory }
 import scala.util.control.Exception.allCatch
 import scala.collection.JavaConverters._
+import scalaz._, Scalaz._
 
 object Config {
 
   implicit lazy val connection: CrowdConnection =
     connectionOpt getOrElse CrowdConnection("http://input.crowd.url.prefix", "input-appname", "input-password")
 
-  implicit lazy val authorizedGroups: Seq[String] =
-    authorizedGroupsOpt getOrElse Seq()
+  implicit lazy val authorizedGroup: AuthorizedGroup =
+    authorizedGroupOpt getOrElse AuthorizedGroup(Seq())
 
   def connectionOpt: Option[CrowdConnection] = for {
     config <- allCatch opt ConfigFactory.load()
@@ -19,11 +20,11 @@ object Config {
     password <- allCatch opt config.getString("crowd4s.password")
   } yield CrowdConnection(urlPrefix, appname, password)
 
-  def authorizedGroupsOpt: Option[Seq[String]] = for {
+  def authorizedGroupOpt: Option[AuthorizedGroup] = for {
     config <- allCatch opt ConfigFactory.load()
     authorizedGroups <- allCatch opt {
       config.getStringList("crowd4s.authorizedGroups")
-        .asScala.toList
+        .asScala.toSeq |> AuthorizedGroup.apply
     }
   } yield authorizedGroups
 
